c2f51ee867f55bddfc8591a9d6555243d8412abb
uslstenn <shurygin0171@gmail.com>
2023-05-19 06:23:26 +0300

    [cpu] datapath reimpl. for pipeline
    hazard in progress
    alu codestyle changes was provided

--- src/pipeline/alu.v
+++ src/pipeline/alu.v
@@ -1,37 +1,37 @@
 `include "alu_consts.v"
 
 module alu(input [31:0] a, b,
-           input [3:0] ALU_op,
-           output reg[31:0] ALU_out,
+           input [3:0] alu_op,
+           output reg[31:0] alu_out,
            output reg zero);
 
   logic [4:0] shamt = b[4:0];
   // always @(*) begin
   always_latch begin 
-                case (ALU_op)
+                case (alu_op)
                          `ALUOP_ADD:
-                             ALU_out = a + b;
+                             alu_out = a + b;
                          `ALUOP_SUB:
-                             ALU_out = a - b;
+                             alu_out = a - b;
                          `ALUOP_AND:
-                             ALU_out = a & b;
+                             alu_out = a & b;
                          `ALUOP_OR:
-                             ALU_out = a | b;
+                             alu_out = a | b;
                          `ALUOP_XOR:
-                             ALU_out = a ^ b;
+                             alu_out = a ^ b;
                          `ALUOP_SLTU:
-                             ALU_out = {{31{1'b0}}, a < b};
+                             alu_out = {{31{1'b0}}, a < b};
                          `ALUOP_SLT:
-                             ALU_out = {{31{1'b0}}, $signed(a) < $signed(b)};
+                             alu_out = {{31{1'b0}}, $signed(a) < $signed(b)};
                          `ALUOP_SLL:
-                             ALU_out = a << shamt;
+                             alu_out = a << shamt;
                          `ALUOP_SRL:
-                             ALU_out = a >> shamt;
+                             alu_out = a >> shamt;
                          `ALUOP_SRA:
-                             ALU_out = $signed(a) >>> shamt;
+                             alu_out = $signed(a) >>> shamt;
                         //! NOTE: by default do nothing
                          default: ;
                 endcase 
-                zero = (ALU_out == 0);
+                zero = (alu_out == 0);
               end
 endmodule
--- src/pipeline/datapath.v
+++ src/pipeline/datapath.v
@@ -10,7 +10,7 @@ module datapath (input clk, reset, hlt,
                  //
                  input reg_writeD, jumpD,
                  input [3:0] alu_controlD,
-                 input alusrc_a_zero,
+                 input alu_src_a_zero,
                  output [31:0] pcF,
                  input [31:0] instrF,
                  output [31:0] alu_outM, write_dataM,
@@ -23,43 +23,238 @@ module datapath (input clk, reset, hlt,
                  output [31:0] instrD
                 );
     //
-    wire [31:0] pcnext, pcnextbr, pcplus4, pcbranch, jump_base, jump_pc, jmp_pc_final;
-    wire [4:0] ra1;
-    wire [31:0] srca, srcb;
-    wire [31:0] imm;
-    wire [31:0] srca, srcb;
-    wire [31:0] result;
+
+    //! -------------------------------------------------------------
+    //! NOTE: fetch stage
+    logic stallF;
+    logic stallD, flushD;
+    logic jumpE;
+    
+    logic [31:0] jmp_fin_pcE;
+    logic [31:0] pcD; 
+    logic [31:0] pcnextF, pcplus4F, pcbranchE, pcnextbrF;
     //
-    //always @(posedge clk)
-    //    if (hlt)
-    //        $finish;
+    pc_reg pcreg(.clk(clk), 
+                 .en(!stallF), 
+                 .clr(reset),
+                 .pc_in(pcnextF), 
+                 .pc_out(pcF)
+                 );
+
+    adder pcadd1(.a(pcF), .b(32'b100), .y(pcplus4F));
+
+    logic pc_srcE;
+
+    mux2 #(32) pcbrmux(.d0(pcplus4F), 
+                       .d1(pcbranchE),
+                       .s(pc_srcE), 
+                       .y(pcnextbrF));
+
+    mux2 #(32) pcmux(.d0(pcnextbrF), 
+                     .d1(jmp_fin_pcE), 
+                     .s(jumpE), 
+                     .y(pcnextF));
+
+    pipereg #(`F2D_WIDTH) Fetch2Decode(.clk(clk), 
+                               .en(!stallD), 
+                               .clr(flushD),
+                               .inp({instrF, pcF}),
+                               .out({instrD, pcD})
+                              );
+
+    //! -------------------------------------------------------------
+    //! NOTE: decode stage
+    logic [31:0] immD, rd1D, rd2D;
+    logic [4:0] ra1D = instrD[19:15] & ~{5{alu_src_a_zeroD}}; 
+    logic [4:0] ra2D = instrD[24:20];; 
+    logic [4:0] rdD = instrD[11:7];
+    logic [4:0] rdW;
+    logic reg_writeW;
+    logic hltE, mem_to_regE, jump_srcE, reg_writeE,
+                mem_writeE, branchE, inv_brE, flushE;
     //
-    ////! NOTE: next PC logic
-    //flopr #(32) pcreg(clk, reset, pcnext, pc);
-    //adder pcadd1 (pc, 32'b100, pcplus4);
-    //adder pcadd2(pc, imm, pcbranch);
-    ////
-    //mux2 #(32) pcbrmux(pcplus4, pcbranch, pcsrc, pcnextbr);
-    //mux2 #(32) jmpscrmux(pc, srca, jump_src, jump_base);
-    //adder jmptar(jump_base, imm, jump_pc);
-    //assign jmp_pc_final = jump_pc & ~1;
-    //mux2 #(32) pcmux(pcnextbr, jmp_pc_final, jump, pcnext);
-    ////
-    //imm_sel imm_sel(instr, imm);
-    //assign ra1 = instr[19:15] & ~{5{alusrc_a_zero}};
+    logic [1:0] alu_srcAE, alu_srcBE;
+    logic [2:0] mem_sizeE;
+    logic [3:0] alu_controlE;
+    logic [4:0] ra1E, ra2E, rdE;
+    logic [31:0] pcE, rd1E, rd2E, immE, instrE, resultW;
+
+    immSel immsel(.instr(instrD), .imm(immD));
     //
-    ////! NOTE: register file logic
-    //regfile rf(clk, ra1, instr[24:20],
-    //           writereg, instr[11:7],
-    //           result, srca, writedata);
-    ////
-    //mux2 #(32) resmux(aluout, readdata, memtoreg, result);
-    ////
-    //// mux2 #(5) wrmux(instr[20:16], instr[15:11], regdst, writereg);
-    //// signext se(instr[15:0], signimm);
+    regfile rf(.clk(clk), 
+               .ra1(ra1D),
+               .ra2(ra2D),
+               .we3(reg_writeW), .wa3(rdW),
+               .wd3(resultW),
+               .rd1(rd1D), .rd2(rd2D)
+              );
     //
-    ////! NOTE: ALU logic
-    //mux4 #(32) srcbmux(writedata, imm, pcbranch, pcplus4, alusrc, srcb);
-    //alu alu(srca, srcb, alucontrol, aluout, zero);
+    pipereg #(`D2E_WIDTH) Decode2Exec(.clk(clk), .en(1), .clr(flushE),
+                               .inp({hltD, 
+                                     mem_to_regD, 
+                                     jump_srcD, 
+                                     alu_srcAD, 
+                                     alu_srcBD, 
+                                     reg_writeD, 
+                                     jumpD, 
+                                     alu_controlD, 
+                                     pcD, 
+                                     mem_writeD, 
+                                     mem_sizeD, 
+                                     branchD, 
+                                     inv_brD, 
+                                     rd1D, 
+                                     rd2D, 
+                                     ra1D, 
+                                     ra2D, 
+                                     rdD, 
+                                     immD, 
+                                     instrD}),
+                                //
+                               .out({hltE,
+                                     mem_to_regE, 
+                                     jump_srcE, 
+                                     alu_srcAE, 
+                                     alu_srcBE,
+                                     reg_writeE, 
+                                     jumpE, 
+                                     alu_controlE, 
+                                     pcE, 
+                                     mem_writeE,
+                                     mem_sizeE, 
+                                     branchE, 
+                                     inv_brE, 
+                                     rd1E, 
+                                     rd2E, 
+                                     ra1E, 
+                                     ra2E, 
+                                     rdE,
+                                     immE, 
+                                     instrE})
+                              );
+    //! -------------------------------------------------------------
+    //! NOTE: execute stage
+    logic [31:0] srcAE, srcBE, alu_outE, rd1Efrw, rd2Efrw;
+    logic [1:0] forwardAE, forwardBE;
+    logic zeroE;
+    logic controlChange;
+    //
+    assign pc_srcE = branchE & (zeroE ^ inv_brE);
+    //! NOTE: indirect branches processing
+    assign controlChange = pc_srcE | jumpE;
+
+    //
+    mux3 #(32) forwardAmux(.d0(rd1E), 
+                           .d1(resultW), 
+                           .d2(alu_outM),
+                           .s(forwardAE), 
+                           .y(rd1Efrw));
+
+    mux3 #(32) forwardBmux(.d0(rd2E), 
+                           .d1(resultW), 
+                           .d2(alu_outM),
+                           .s(forwardBE), 
+                           .y(rd2Efrw));
+
+
+    mux2 #(32) srcamux(.d0(rd1Efrw), 
+                       .d1(pcE),
+                       .s(alu_srcAE[0]), 
+                       .y(srcAE));
+
+    mux3 #(32) srcbmux(.d0(rd2Efrw), 
+                       .d1(immE),
+                       .d2(32'd4),
+                       .s(alu_srcBE), 
+                       .y(srcBE));
+
+    alu alu(.a(srcAE), 
+            .b(srcBE),
+            .alu_op(alu_controlE), 
+            .alu_out(alu_outE),
+            .iszero(zeroE));
+
+    logic [31:0] jmp_baseE, jmp_pcE;
+    adder pcaddimm(.a(pcE), .b(immE), .y(pcbranchE));
+
+    mux2 #(32) jmpsrcmux(.d0(pcE), .d1(rd1E),
+                         .s(jump_srcE), .y(jmp_baseE));
+
+    adder jmptar(.a(jmp_baseE), .b(immE), .y(jmp_pcE));
+    
+    assign jmp_fin_pcE = jmp_pcE & ~1;
+    logic [31:0] write_dataE;
+    assign write_dataE = rd2Efrw;
+
+    logic reg_writeM, mem_to_regM, hltM, controlChangeM;
+    logic [4:0] rdM;
+    logic [31:0] pcM, instrM;
+
+    pipereg #(`E2M_WIDTH) Exec2Memory(.clk(clk), 
+                               .en(1), 
+                               .clr(reset),
+                               .inp({reg_writeE, 
+                                     mem_to_regE,  
+                                     mem_writeE,
+                                     hltE, 
+                                     mem_sizeE, 
+                                     rdE, 
+                                     write_dataE, 
+                                     alu_outE,
+                                     pcE, 
+                                     controlChange, 
+                                     instrE}),
+                                //
+                               .out({reg_writeM, 
+                                     mem_to_regM, 
+                                     mem_writeM,
+                                     hltM, 
+                                     mem_sizeM, 
+                                     rdM, 
+                                     write_dataM, 
+                                     alu_outM, 
+                                     pcM, 
+                                     controlChangeM, 
+                                     instrM}));
+
+    //! -------------------------------------------------------------
+    //! NOTE: memory stage
+    logic mem_to_regW, mem_writeW, validW, controlChangeW, hltW;
+    logic [31:0] alu_outW, read_dataW, write_dataW, pcW, instrW;
+
+    pipereg #(`M2W_WIDTH) Memory2Write(.clk(clk), 
+                                .en(1), 
+                                .clr(reset),
+                                .inp({reg_writeM, 
+                                      mem_to_regM, 
+                                      hltM, 
+                                      rdM, 
+                                      alu_outM, 
+                                      read_dataM, 
+                                      mem_writeM, 
+                                      write_dataM, 
+                                      pcM != 0, 
+                                      pcM, 
+                                      controlChangeM, 
+                                      instrM}),
+                                //
+                                .out({reg_writeW, 
+                                      mem_to_regW, 
+                                      hltW, 
+                                      rdW, 
+                                      alu_outW, 
+                                      read_dataW, 
+                                      mem_writeW, 
+                                      write_dataW, 
+                                      validW, 
+                                      pcW, 
+                                      controlChangeW, 
+                                      instrW}));
+
+    //! -------------------------------------------------------------
+    //! NOTE: writeback stage
+    mux2 #(32) resmux(.d0(alu_outW), .d1(read_dataW),
+                      .s(mem_to_regW), .y(resultW));
+    reg [31:0] num = 0;
 //
 endmodule
